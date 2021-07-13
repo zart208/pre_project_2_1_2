f1cee2c721920d6bc3999c3eb4ef6caf63ad2333
Artem Zakharov <zart208@gmail.com>
2021-07-13 22:50:44 +0300

    task_2_1_2 for review

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,13 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat1.getName());
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat2.getName());
+        System.out.println(bean == bean2);
+        System.out.println(beanCat1 == beanCat2);
     }
 }
